8c243ce1960955217cab7b43a23e7563efe67d79
wagagaha <onetime-depths0l@icloud.com>
2023-07-28 11:55:52 +0800

    feat: add integrations: pwa, compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,56 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import AstroPWA from '@vite-pwa/astro'
+import vercel from '@astrojs/vercel/serverless'
+import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
+  integrations: [
+    tailwind(), 
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'wagagaha',
+        short_name: 'wagagaha',
+        start_url:'/',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ]
+      },
+      workbox: {
+        navigateFallback: '/404',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/404$/],
+      },
+    }), 
+    compress()
+  ],
+  output: 'server',
   adapter: vercel({
-    analytics: true,
-  }),
-});
+    analytics: true
+  })
+})
