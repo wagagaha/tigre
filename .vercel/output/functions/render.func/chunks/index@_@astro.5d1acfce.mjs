8544d0b11e3822885edfc34c7df493797027075b
wagagaha <onetime-depths0l@icloud.com>
2023-07-31 16:54:49 +0800

    refactor: responsible ui for navigation

---
+++ .vercel/output/functions/render.func/chunks/index@_@astro.5d1acfce.mjs
@@ -0,0 +1,7 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.d9f4bca8.mjs';
+
+const page = () => import('./pages/index.astro.a9a25f39.mjs');
+
+export { page };
--- astro.config.ts
+++ astro.config.ts
@@ -8,11 +8,6 @@ import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-      external: ['svgo']
-    },
-  },
   integrations: [
     tailwind(), 
     AstroPWA({
