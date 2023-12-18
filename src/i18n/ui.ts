61ea4c4d42b0147debdccf239e91e07154bf2830
wagagaha <onetime-depths0l@icloud.com>
2023-12-18 22:03:13 +0800

    feat: add i18n(zh&en) support

---
+++ src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+  en: 'English',
+  zh: '简体中文',
+};
+
+export const defaultLang = 'en';
+export const showDefaultLang = false;
+
+export const ui: any = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.blog': 'Blog',
+    'nav.project': 'Project'
+  },
+  zh: {
+    'nav.home': '首页',
+    'nav.about': '关于',
+    'nav.blog': '博客',
+    'nav.project': '项目'
+  },
+} as const;
